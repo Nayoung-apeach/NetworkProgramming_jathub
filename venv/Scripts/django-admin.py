1a4c3f68ccffd4f4ad0d16ae077e69a4f701d9fe
s2019w04 <s2019w04@e-mirim.hs.kr>
2021-05-26 09:16:59 +0900

    Initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\user\documents\pycharmproject\networkprogramming\jathub\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
