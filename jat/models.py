a58c1580d55cc9e467ed3635d03f51e5ad19dc59
s2019w04 <s2019w04@e-mirim.hs.kr>
2021-05-26 10:20:18 +0900

    jat/models verbose_name_plural, deadline, access :D

--- jat/models.py
+++ jat/models.py
@@ -4,10 +4,14 @@ from django.db import models
 class Repository(models.Model):
     name = models.CharField(max_length=40)
     description = models.CharField(max_length=200)
+    deadline = models.DateTimeField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     # repo1.introduction_set.all => 나와관련된 자소서 다 가지고 오는 것 all 생략해도 됨.(그럼 전부 다 가져오는것이 아님)
+
+    class Meta:
+        verbose_name_plural = 'Repositories'
 
     def __str__(self):
         return self.name
@@ -17,6 +21,7 @@ class Introduction(models.Model):
     repository = models.ForeignKey(Repository, on_delete=models.CASCADE)  # intro1.repository idx랑 연결하는 것과 똑같은 역활을 한다.
     version = models.IntegerField(default=1)
     contents = models.TextField()
+    access = models.IntegerField(default=1) #0이면 private 1이면 public
 
     # intro1.comment_set
 
