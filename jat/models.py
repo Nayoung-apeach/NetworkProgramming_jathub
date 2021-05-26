b15f3eeee2982d663ee63b597bb5ba71b2f61d92
s2019w04 <s2019w04@e-mirim.hs.kr>
2021-05-26 10:08:34 +0900

    jat/models Repository, Introduction, Comment :D

--- jat/models.py
+++ jat/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Repository(models.Model):
+    name = models.CharField(max_length=40)
+    description = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    # repo1.introduction_set.all => 나와관련된 자소서 다 가지고 오는 것 all 생략해도 됨.(그럼 전부 다 가져오는것이 아님)
+
+    def __str__(self):
+        return self.name
+
+
+class Introduction(models.Model):
+    repository = models.ForeignKey(Repository, on_delete=models.CASCADE)  # intro1.repository idx랑 연결하는 것과 똑같은 역활을 한다.
+    version = models.IntegerField(default=1)
+    contents = models.TextField()
+
+    # intro1.comment_set
+
+    def __str__(self):
+        return f'{self.version}{self.contents}'
+
+
+class Comment(models.Model):
+    introduction = models.ForeignKey(Introduction, on_delete=models.CASCADE)  # comment1.introduction
+    comment = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.comment
