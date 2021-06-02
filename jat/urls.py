30bedcc6d3e8aac6d8cc3009e7aa2418b7043238
s2019w04 <s2019w04@e-mirim.hs.kr>
2021-06-02 09:15:10 +0900

    jat/ urls introduction_detail :D

--- jat/urls.py
+++ jat/urls.py
@@ -5,6 +5,8 @@ from jat import views
 app_name = 'jat'
 
 urlpatterns = [
-    path('', views.RepositoryListView.as_view(), name='repository_list'), #jat:repository_list -> path의 풀네임
+    path('', views.RepositoryListView.as_view(), name='repository_list'),  # jat:repository_list -> path의 풀네임
     path('repository/<int:pk>/', views.RepositoryDetailView.as_view(), name='repository_detail'),
+    path('repository/<int:repository_pk>/introduction/<int:pk>/', views.IntroductionDetailView.as_view(),
+         name='introduction_detail'),
 ]
