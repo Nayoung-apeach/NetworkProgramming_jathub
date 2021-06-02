e3a6bf58b83614314493e740ae821cd0cdea724c
s2019w04 <s2019w04@e-mirim.hs.kr>
2021-06-02 10:15:36 +0900

    jat/urls repositoru_modify, repository_delete:D

--- jat/urls.py
+++ jat/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('repository/<int:repository_pk>/introduction/<int:pk>/', views.IntroductionDetailView.as_view(),
          name='introduction_detail'),
     path('repository/add/', views.RepositoryCreateView.as_view(), name='repository_add'),
-
+    path('repository/<int:pk>/modify', views.RepositoryUpdateView.as_view(), name='repository_modify'),
+    path('repository/<int:pk>/delete', views.RepositoryDeleteView.as_view(), name='repository_delete'),
 ]
